c61b43748ab0b06fc140a056b448b1938b5128c8
bharat19334 <YOUR_GITHUB_EMAIL>
2026-06-06 00:06:43 +0530

    added new questions

--- LeetcodeDSA/55. Jump Game.py
+++ LeetcodeDSA/55. Jump Game.py
@@ -6,6 +6,6 @@ def canJump(nums):
             return False
         max_reach = max(max_reach, i + nums[i])
     return True
-        
-nums = [2,3,1,1,4]
+
+nums = [2,4,1,1,0,3]
 print(canJump(nums))
